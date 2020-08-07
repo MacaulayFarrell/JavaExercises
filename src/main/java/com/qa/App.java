a3c17299d962a7b8f0e6cae5531e972379e071b6
Macaulay Farrell <Maccpr7@gmail.com>
2020-08-07 16:08:03 +0100

    add method to print value

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -1,13 +1,26 @@
 package com.qa;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
+    public static String printMessage()
+    {
+        String message = "Using method";
+        return message;
+    }
+
     public static void main( String[] args )
     {
+        //declare variable and print it out
+        String hello = "Hello World";
+        System.out.println(hello);
+        //print something to console
         System.out.println( "Hello Everyone" );
+        //get result of method
+        printMessage();
+        //changing value of a variable declared
+        hello = printMessage();
+        System.out.println(hello);
+
     }
+
 }
