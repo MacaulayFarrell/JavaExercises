d51b98a27d4a77225bf02d3823abc7dc09ff659e
Macaulay Farrell <Maccpr7@gmail.com>
2020-08-10 16:01:37 +0100

    add exercise 1 for conditionals

---
+++ src/main/java/com/qa/day2/Exercise2.java
@@ -0,0 +1,19 @@
+package com.qa.day2;
+
+public class Exercise2 {
+    public static int num1 = 5;
+    public static int num2 = 5;
+    public static boolean state = true;
+    public static void flow(int num1, int num2, boolean state) {
+        if(state) {
+            System.out.println(num1 + num2);
+        }
+        else {
+            System.out.println(num1 * num2);
+        }
+    }
+
+    public static void main(String[] args) {
+        flow(num1, num2, state);
+    }
+}
