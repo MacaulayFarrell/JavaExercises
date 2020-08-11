392fb78afdfc0f5d7c5ebcaf6e8538f80949997b
Macaulay Farrell <Maccpr7@gmail.com>
2020-08-11 12:14:50 +0100

    loop created to print 1-10 ten times

---
+++ src/main/java/com/qa/day3/Exercise1.java
@@ -0,0 +1,22 @@
+package com.qa.day3;
+
+public class Exercise1 {
+
+    public static void loop1() {
+        //printing 1-10 10 times
+        System.out.println("Printing 1-10 ten times.");
+        for(int i = 0; i <= 10; i++) {
+            for(int j = 1; j <= 10; j++) {
+                System.out.println(j);
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        loop1();
+
+
+    }
+}
