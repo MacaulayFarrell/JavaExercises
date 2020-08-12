04ae86da8fbe53f762efdaca01ab9c5a9b4a4925
Macaulay Farrell <Maccpr7@gmail.com>
2020-08-12 12:37:06 +0100

    update exercise

--- src/main/java/com/qa/day4/Factorials.java
+++ src/main/java/com/qa/day4/Factorials.java
@@ -3,7 +3,8 @@ package com.qa.day4;
 import java.util.Scanner;
 
 public class Factorials {
-    public static void main(String[] args) {
+    public static void reverse() {
+        //given a number this will get the reverse of Factorial
         Scanner userInput = new Scanner(System.in); //scanners are used to get user input in console
         System.out.println("PLease enter a number: ");
         int storeInput = userInput.nextInt(); //stores the scanner input from user, .nextInt(); means input must be an Int.
@@ -17,6 +18,11 @@ public class Factorials {
         else { // otherwise print the reverse of factorial
             System.out.println("The reverse of the factorial of " + storeInput + " is " + result);
         }
+    }
+
+    public static void main(String[] args) {
+        reverse();
+
 
 
     }
