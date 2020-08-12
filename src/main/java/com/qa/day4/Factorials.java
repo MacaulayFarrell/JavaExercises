d71086b2eb603b28f9696ab5e324f9ffbc2a16ec
Macaulay Farrell <Maccpr7@gmail.com>
2020-08-12 12:33:53 +0100

    add reverse of Factorial exercise

---
+++ src/main/java/com/qa/day4/Factorials.java
@@ -0,0 +1,23 @@
+package com.qa.day4;
+
+import java.util.Scanner;
+
+public class Factorials {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in); //scanners are used to get user input in console
+        System.out.println("PLease enter a number: ");
+        int storeInput = userInput.nextInt(); //stores the scanner input from user, .nextInt(); means input must be an Int.
+        int result = storeInput;
+        for(int i = 2; result != i; i++) {
+            result = result / i;
+        }
+        if(result == 0) { //ensures that if there is no factorial then it will print NONE
+            System.out.println("NONE");
+        }
+        else { // otherwise print the reverse of factorial
+            System.out.println("The reverse of the factorial of " + storeInput + " is " + result);
+        }
+
+
+    }
+}
