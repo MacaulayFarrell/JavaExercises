b56b6447fc6688e96a3672fd2e41f10b580e72ec
Macaulay Farrell <Maccpr7@gmail.com>
2020-08-18 13:16:36 +0100

    add HashSets exercise

---
+++ src/main/java/com/qa/day6/HashSets.java
@@ -0,0 +1,26 @@
+package com.qa.day6;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSets {
+    public static void hashSet() {
+        Set<String> hashSet = new HashSet<>();
+        hashSet.add("hello");
+        hashSet.add("welcome");
+        hashSet.add("doing it");
+        System.out.println("Size of hashSet: " + hashSet.size());
+        System.out.println("Is the set empty? " + hashSet.isEmpty());
+        hashSet.remove("hello");
+
+        for(String i : hashSet) {
+            System.out.println("Element: " + i);
+        }
+
+
+    }
+    public static void main(String[] args) {
+        hashSet();
+
+    }
+}
